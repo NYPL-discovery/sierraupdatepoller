943499305523afc51949f60f27ff857d2cc21390
Jobin Thomas <jobinthomas@nypl.org>
2017-02-15 17:52:08 -0500

    Send items one by one

--- src/main/java/org/nypl/harvester/sierra/processor/StreamPoster.java
+++ src/main/java/org/nypl/harvester/sierra/processor/StreamPoster.java
@@ -1,5 +1,6 @@
 package org.nypl.harvester.sierra.processor;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.Map;
@@ -7,7 +8,10 @@ import java.util.Map;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.ProducerTemplate;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.nypl.harvester.sierra.exception.SierraHarvesterException;
 import org.nypl.harvester.sierra.model.Item;
 import org.nypl.harvester.sierra.utils.HarvesterConstants;
 import org.slf4j.Logger;
@@ -27,25 +31,42 @@ public class StreamPoster implements Processor{
 	public void process(Exchange exchange) throws Exception {
 		Map<String, Object> exchangeContents = exchange.getIn().getBody(Map.class);
 		List<Item> items = (List<Item>) exchangeContents.get(HarvesterConstants.APP_ITEMS_LIST);
-		sendToKinesis(new ObjectMapper().writeValueAsString(items));
+		sendToKinesis(items);
 		logger.info("Sent " + items.size() + " items to kinesis");
 	}
 	
-	private void sendToKinesis(String content){
-		ByteBuffer byteBuffer = ByteBuffer.wrap(content.getBytes());
-		Exchange kinesisResponse = template.send("aws-kinesis://" + 
-				System.getenv("kinesisStream")
-				+ "?amazonKinesisClient=#getAmazonKinesisClient", new Processor() {
-					
-					@Override
-					public void process(Exchange kinesisRequest) throws Exception {
-						kinesisRequest.getIn().setHeader(HarvesterConstants.KINESIS_PARTITION_KEY, 
-								Integer.parseInt(System.getenv("KinesisPartitionKey")));
-						kinesisRequest.getIn().setHeader(HarvesterConstants.KINESIS_SEQUENCE_NUMBER,
-								System.currentTimeMillis());
-						kinesisRequest.getIn().setBody(byteBuffer);
-					}
-				});
+	private void sendToKinesis(List<Item> items) throws SierraHarvesterException{
+		try{
+			for(Item item : items){
+				String content = new ObjectMapper().writeValueAsString(item);
+				ByteBuffer byteBuffer = ByteBuffer.wrap(content.getBytes());
+				Exchange kinesisResponse = template.send("aws-kinesis://" + 
+						System.getenv("kinesisStream")
+						+ "?amazonKinesisClient=#getAmazonKinesisClient", new Processor() {
+							
+							@Override
+							public void process(Exchange kinesisRequest) throws Exception {
+								kinesisRequest.getIn().setHeader(HarvesterConstants.KINESIS_PARTITION_KEY, 
+										Integer.parseInt(System.getenv("KinesisPartitionKey")));
+								kinesisRequest.getIn().setHeader(HarvesterConstants.KINESIS_SEQUENCE_NUMBER,
+										System.currentTimeMillis());
+								kinesisRequest.getIn().setBody(byteBuffer);
+							}
+						});
+			}
+		}catch(JsonGenerationException jsonGenerationException){
+			logger.error("Hit json generation exception while converting item to json - ", 
+					jsonGenerationException);
+			throw new SierraHarvesterException("Hit an exception while processing item");
+		}catch(JsonMappingException jsonMappingException){
+			logger.error("Hit json mapping exception while converting item to json - ", 
+					jsonMappingException);
+			throw new SierraHarvesterException("Hit an exception while processing item");
+		}catch(IOException ioException){
+			logger.error("Hit IOException while converting item to json - ", 
+					ioException);
+			throw new SierraHarvesterException("Hit an exception while processing item");
+		}
 	}
 
 }
