7b4fe12a4b4563d5f66a08a8a29572ec7fab72df
Kevin Friedman <kevinfriedman@nypl.org>
2017-02-28 17:22:05 -0500

    DISC-24 Added README and renamed appplication class

--- src/main/java/org/nypl/harvester/sierra/SierraItemUpdatePoller.java
+++ src/main/java/org/nypl/harvester/sierra/SierraItemUpdatePoller.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SierraItemIdPollerApp {
+public class SierraItemUpdatePoller {
 
   public static void main(String[] args) {
-    SpringApplication.run(SierraItemIdPollerApp.class, args);
+    SpringApplication.run(SierraItemUpdatePoller.class, args);
   }
 }
