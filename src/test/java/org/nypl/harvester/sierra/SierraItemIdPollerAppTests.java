20006081406321771d5858d8a2a645ac7bd72ce7
Jobin Thomas <jobinthomas@nypl.org>
2017-08-15 13:32:03 -0400

    Ran code checkstyle formatting

--- src/test/java/org/nypl/harvester/sierra/SierraItemIdPollerAppTests.java
+++ src/test/java/org/nypl/harvester/sierra/SierraItemIdPollerAppTests.java
@@ -1,15 +1,15 @@
-//package org.nypl.harvester.sierra;
+// package org.nypl.harvester.sierra;
 //
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 //
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class SierraItemIdPollerAppTests {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class SierraItemIdPollerAppTests {
 //
-//  @Test
-//  public void contextLoads() {}
+// @Test
+// public void contextLoads() {}
 //
-//}
+// }
